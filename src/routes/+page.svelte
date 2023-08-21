da9becbf59dec2e5d775c1ed4d234f1ed87970e1
Steffen Glaeser <steffen.hauss@gmail.com>
2023-08-21 21:33:26 +0200

    build: publish workflow

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<!-- <h1>development<sup>guide</sup></h1>
+
+<footer>Test</footer> -->
+
+<style>
+	h1 {
+		margin: 0 auto;
+		text-align: center;
+	}
+
+	footer {
+		background-color: oklch(33.48% 0.039 284.13);
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		text-align: center;
+		font-size: 1rem;
+		color: #f2edeb;
+		padding: 10px 0;
+	}
+</style>
