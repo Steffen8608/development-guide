f4bf521d76435d8bb493b2bed47dbadd8a1b5b09
Steffen Glaeser <steffen.hauss@gmail.com>
2023-08-22 21:44:27 +0200

    feat: add auth

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import { signOut } from 'firebase/auth';
+	import { firebaseAuth } from '$lib/utils/firebase';
+	import { authUser } from '$lib/stores/auth.store';
+	import Analytics from '$lib/components/Analytics.svelte';
+
+	const handleLogout = () => {
+		signOut(firebaseAuth)
+			.then(() => {
+				$authUser = undefined;
+				goto('/login');
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+</script>
+
+<Analytics />
+
+<header class="space-y-4">
+	<a href="/" class="font-bold hover:underline">Home</a>
+
+	<nav class="flex gap-4">
+		{#if $authUser}
+			<a
+				href="/protected"
+				class="hover:underline"
+				class:active={$page.url.pathname === '/protected'}>Protected</a
+			>
+			<button class="hover:underline" on:click={handleLogout}>Logout</button>
+		{:else}
+			<a href="/register" class="hover:underline" class:active={$page.url.pathname === '/register'}
+				>Register</a
+			>
+			<a href="/login" class="hover:underline" class:active={$page.url.pathname === '/login'}
+				>Login</a
+			>
+		{/if}
+	</nav>
+</header>
+<slot />
